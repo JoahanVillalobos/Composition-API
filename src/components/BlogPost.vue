de27fb5b90962b3d76c4ccfa3cf8167f02d9cd20
JoahanVillalobos <joahan.ortega@cceo.com.mx>
2024-02-29 18:09:54 -0600

    feature (BlogPost.vue): :sparkles: add component BlogPost

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+defineProps(['title', 'id', 'body', 'colorText'])
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 :class="`text-${colorText}`" class="card-title">{{ id }} - {{ title }}</h5>
+            <p>{{ body }}</p>
+        </div>
+    </div>
+</template>
+
+<style></style>
