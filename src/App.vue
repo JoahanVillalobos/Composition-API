8c31a9103a7bd3819219f1424b0e1d77b617cff1
JoahanVillalobos <joahan.ortega@cceo.com.mx>
2024-02-29 18:50:09 -0600

    feature (src/App.vue src/components/BlogPost.vue): :sparkles: using v-for to run an array called posts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script setup>
-import ButtonCounter from './components/ButtonCounter.vue'
+import { ref } from 'vue'
 import BlogPost from './components/BlogPost.vue'
+const posts = ref([
+  { title: "Post 1", id: 1, body: "Descripción 1" },
+  { title: "Post 2", id: 2, body: "Descripción 2" },
+  { title: "Post 3", id: 3, body: "Descripción 3" },
+  { title: "Post 4", id: 4 },
+])
+
 </script>
 
 <template>
   <div class="container">
     <h1>APP</h1>
-    <ButtonCounter />
-    <button-counter></button-counter>
-    <BlogPost title="Post 1" id="1" body="descripcion 1" colorText="primary" />
-    <BlogPost title="Post 2" id="2" body="descripcion 2" colorText="secondary" />
-    <BlogPost title="Post 3" id="3" body="descripcion 3" colorText="success" />
-    <BlogPost title="Post 3" id="4" body="descripcion 4" colorText="primary" />
+    <BlogPost v-for="post in posts" :key="post.id" :title="post.title" :id="post.id" :body="post.body" />
   </div>
 </template>
 
--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,13 +1,21 @@
 <script setup>
 
-defineProps(['title', 'id', 'body', 'colorText'])
+//defineProps(['title', 'id', 'body'])
+defineProps({
+    title: String,
+    id: Number,
+    body: {
+        type: String,
+        default: "Sin descripcion"
+    }
+})
 
 </script>
 
 <template>
     <div class="card">
         <div class="card-body">
-            <h5 :class="`text-${colorText}`" class="card-title">{{ id }} - {{ title }}</h5>
+            <h5 class="card-title">{{ id }} - {{ title }}</h5>
             <p>{{ body }}</p>
         </div>
     </div>
